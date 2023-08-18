11c2730f95b1e52c17872c8493b02cdaf2620e0a
EmySilv <emiisilva1978@gmail.com>
2023-08-18 16:00:49 -0300

    Aulas 4 e 5, 22.08.2023

---
+++ JS/main.js
@@ -0,0 +1,27 @@
+/*JavaScript #04 - Map, Filter & Reduce
+const numbers = [1, 2, 3, 4, 5, 6];
+const numbersMultiplye = numbers.map(function(number){
+    return number * 2;
+});
+
+console.log(numbersMultiplye);
+
+const ages = [1, 18, 32, 22, 40, 55, 30];
+
+console.log("===============================")
+
+// const evenAges = ages.filter(function (age) {
+//     return age % 2 === 0;
+// });
+
+const sumOfAges = ages.reduce(function(age, accumulator){
+    return accumulator + age;
+}, 0);
+
+console.log(sumOfAges);
+
+console.log("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
+*/
+
+
+/*JavaScript #05 - Objetos*/
