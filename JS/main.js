c152d372756c6b8b100474e14b991598c9cdc40b
EmySilv <emiisilva1978@gmail.com>
2023-08-22 19:45:00 -0300

    aula 6, JSON

--- JS/main.js
+++ JS/main.js
@@ -24,4 +24,75 @@ console.log("|||||||||||||||||||||||||||||||||||||||||||||||||||||||||")
 */
 
 
-/*JavaScript #05 - Objetos*/
+/*JavaScript #05 - Objetos
+const person = {
+    firstName: "emy",
+    lastName: "silv",
+    age: 18,
+    hobbies: ["ler", "comer", "jogar"],
+    dog: {
+        name: "simba",
+        age: "1"
+    }
+};
+
+// const firstName = person.firstName;
+// const lastName = person.lastName;
+// const age = person.age;
+// const hobbies = person.hobbies;
+
+
+const {firstName: primeiroNome, lastName, age, hobbies, dog: {name}} = person;
+
+const read = person.hobbies[2];
+
+console.log(firstName);
+console.log(lastName);
+console.log(age);
+console.log(hobbies);
+
+
+const todos = [
+    {
+        id: 1,
+        description: 'estudar programação nessa caraia',
+        isCompleted: false
+    },
+    {
+        id: 2,
+        description: 'estudar programação nessa caraia',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        description: 'comer',
+        isCompleted: true
+    },
+];
+
+const object = todos[1].description;
+
+console.log(object)*/
+
+
+/*JavaScript #06 - JSON*/
+const todos = [
+    {
+        id: 1,
+        description: 'estudar programação nessa caraia',
+        isCompleted: false
+    },
+    {
+        id: 2,
+        description: 'estudar programação nessa caraia',
+        isCompleted: true
+    },
+    {
+        id: 3,
+        description: 'comer',
+        isCompleted: true
+    },
+];
+
+const todosJSON = JSON.stringify(todos);
+const todosList = JSON.parse(todosJSON);
